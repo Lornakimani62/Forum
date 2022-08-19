a23ee2f7904639060c4aacac68260ed981fdf4f2
Lorna Kimani <kimanilorna@yahoo.com>
2022-08-19 18:55:15 +0300

    Redirect unauthenticated user

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,14 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        if(!auth()->check())
+        {
+            if ($request->ajax())
+            {
+
+                return redirect()->guest('auth/login');
+
+            }
+    }
     }
 }
