680f1a9132d73d1c34d54a5455b30226e12d3e2e
Lorna Kimani <kimanilorna@yahoo.com>
2022-08-19 18:58:20 +0300

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RemarkController;
+use App\Http\Controllers\CustomAuthController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Routes for logged in users.
+$router->group(['middleware' => ['auth']], function($router) {
+    Route::get('dashboard', [RemarkController::class, 'index'])->name('dashboard'); 
+    Route::get('create', [RemarkController::class, 'create'])->name('create'); 
+    
 });
+
+Route::get('/', [CustomAuthController::class, 'index']);
+Route::post('store', [RemarkController::class, 'store'])->name('store'); 
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
